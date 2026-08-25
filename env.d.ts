0acb0edbc54fa1c604caf0aee9a354011c42d030
babas <babassebastien02@gmail.com>
2026-08-25 10:04:55 +0400

    migration de loops à plunk et mise à jours

--- env.d.ts
+++ env.d.ts
@@ -4,3 +4,14 @@ declare module 'virtual:starlight/components/LastUpdated';
 declare module 'virtual:starlight/components/Pagination';
 declare module 'virtual:starlight/user-config';
 declare module '@astrojs/starlight/props';
+
+
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_API_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
